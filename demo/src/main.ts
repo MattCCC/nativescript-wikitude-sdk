70307590f00c7eeba150f43bf2b6b807b032e95d
Matt Czapliński <deindesign.pl@gmail.com>
2021-10-30 01:18:38 +0200

    chore: Demo cleanup

--- demo/src/main.ts
+++ demo/src/main.ts
@@ -1,7 +1,3 @@
-// import { NativeScriptDebugIOS } from '@nativescript/debug-ios';
-
-// NativeScriptDebugIOS.show();
-
 import {
   Trace,
   TraceErrorHandler,
@@ -23,14 +19,7 @@ if (Trace && Trace.setErrorHandler) {
 
 import Vue from 'nativescript-vue'
 import App from './components/App.vue'
-// import VueDevtools from 'nativescript-vue-devtools'
 
-// if(TNS_ENV !== 'production') {
-//   Vue.use(VueDevtools, { host: '192.168.0.22' })
-// }
-
-// Prints Vue logs when --env.production is *NOT* set while building
-// Vue.config.silent = (TNS_ENV === 'production') ? true : false;
 Vue.config.silent = false;
 
 (<any>global).wikitudeLicense = "gEN8IfezW61Dm4kcxfllwftvGvSG7jIazp30cQbskywPzUwcQACoefmlYaKMQ9J2MQGZH25WhCAc3ESLBiOd54HPlnJcONQvRd8dYVCakag2G1rRhiA+WA8pP8l7fxhShf4TSeOv8jyYlM+Aey5K+EMGqAV4ikLxK+CiLfwxTC9TYWx0ZWRfX7kKhcv3nzfg7F58KktKsEhtmmccKuzCAQIlStexV+EmbFx1l6FXJA4WSPT9iuxr9Iz3wguRrNztwNeWav9r4SI4iuUgnu3e6/KWO1VJmrBsZbkMRFXaX0HIwOxMBESqSEEFTBB+2bNwaQI5NlHaMHIdsr054UkWHMZUfOHyq6y2kWpQ0acRVwMMCIgbsGFiKlYwjGp6eBcfE525pknd5PmX+/Dgbg9X+dlSxNnhiIjgOZRTtEHOiy0G8XUkDkA2rKrfgeJxSKYHbxd7TdfLofyc9f3hxDmh15P4YzFonN3oUOjutL9rASx667Mx1MgM1KfLmVwN3KHYdRY2LOMt/EKlArnj7tE7XnCR9Nom4Bg52bTfA6PrFUSdxuYu1KwwERWMALePHMdOFjoDwpK9/E65fGLfX3AovR3Wka31HpbcjKrIr2eh5q7TmFDmpX2/YphHnZJHJVCJyQPta/DVpktS1Hm/2Ieaui2nWVekfQYzTw6TDH7Q4rF6evqYKz4dceAIqB+bVhCscmfE/VvNYRrPkHO0SKzWVqS30VBSb9eZDVigGYTNnpCelET//NrbUwCBq49rbydHg0ur/i/wMVC2+gxf8z0/i2xdU3CCe9Z9vIr7eKBsSIeawhvy8cKjFBIWVr6Sf6bL/lVibXOHs/PqDuMkKH+sNyGfqMBaPLjVhNG6lZv3Oqk=";
